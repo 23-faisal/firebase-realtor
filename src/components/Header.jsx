e73403c530b0ff2b36834845a4bb6dbee33f5cc7
Faisal Ahmed <a.faisal23r@gmail.com>
2024-01-16 02:06:02 +0600

    fixed some error in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,11 +6,11 @@ const Header = () => {
   const pathMatch = (route) => {
     if (route === location.pathname) return true;
   };
-  console.log(location);
+
   return (
     <div className="bg-white border-b shadow-sm sticky top-0 z-50 ">
       <header className="flex items-center justify-between px-3 max-w-6xl mx-auto">
-        <div className="cursor-pointer " onClick={() => navigate("/")}>
+        <div onClick={() => navigate("/")}>
           <img
             className="h-6 cursor-pointer "
             src="https://cdn.worldvectorlogo.com/logos/realtor-3.svg"
@@ -18,24 +18,30 @@ const Header = () => {
           />
         </div>
         <div>
-          <ul className="flex items-center space-x-4 md:space-x-10">
+          <ul className="flex items-center space-x-3 md:space-x-10">
             <li
-              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent transition duration-200 ease-in-out ${
-                pathMatch("/") && "text-black border-b-red-500 "
+              className={`py-3 text-sm font-semibold  border-b-[3px] transition duration-200 ease-in-out ${
+                pathMatch("/")
+                  ? "text-black border-b-red-500 "
+                  : "border-b-transparent text-gray-400"
               }`}
             >
               <Link to={"/"}>Home</Link>
             </li>
             <li
-              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent transition duration-200 ease-in-out ${
-                pathMatch("/offers") && "text-black border-b-red-500 "
+              className={`py-3 text-sm font-semibold  border-b-[3px] transition duration-200 ease-in-out ${
+                pathMatch("/offers")
+                  ? "text-black border-b-red-500 "
+                  : "border-b-transparent text-gray-400"
               }`}
             >
               <Link to={"/offers"}>Offers</Link>
             </li>
             <li
-              className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent transition duration-200 ease-in-out ${
-                pathMatch("/sign-in") && "text-black border-b-red-500 "
+              className={`py-3 text-sm font-semibold border-b-[3px] transition duration-200 ease-in-out ${
+                pathMatch("/sign-in")
+                  ? "text-black border-b-red-500 "
+                  : "text-gray-400 border-b-transparent "
               }`}
             >
               <Link to={"/sign-in"}>Sign in</Link>
