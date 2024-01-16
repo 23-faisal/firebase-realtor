86bfe7c4686f3494bf81c5c67a9a801b7fba5318
Faisal Ahmed <a.faisal23r@gmail.com>
2024-01-16 16:04:12 +0600

    complete sign in page funtionally

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import SignInWithGoogle from "../components/SignInWithGoogle";
+import { toast } from "react-toastify";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/firebase.config";
 
 const SignIn = () => {
+  const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
@@ -19,6 +23,22 @@ const SignIn = () => {
     });
   };
 
+  const OnSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      if (userCredential.user) {
+        navigate("/");
+      }
+    } catch (err) {
+      toast.error("Something went wrong!!! Please try again later.");
+    }
+  };
+
   return (
     <section className="max-w-6xl mx-auto px-3">
       <h1 className="text-3xl mt-6 text-center font-bold  ">Sign In</h1>
@@ -31,7 +51,7 @@ const SignIn = () => {
           />
         </div>
         <div className="w-full md:w-[33%] lg:w-[50%] md:ml-12 lg:ml-20 ">
-          <form className="flex flex-col  gap-6">
+          <form onSubmit={OnSubmit} className="flex flex-col  gap-6">
             <input
               className="w-full rounded-md px-2 py-4 text-xl text-gray-700 bg-white border-gray-300 transition duration-150 ease-in-out"
               type="email"
@@ -63,38 +83,39 @@ const SignIn = () => {
                 />
               )}
             </div>
+
+            <div className="md:flex  md:items-center md:justify-between md:mt-3 mt-3">
+              <p className="text-md">
+                <span className="text-gray-500 ">
+                  Don&apos;t have an account?
+                </span>
+                <Link
+                  className="text-red-500 ml-2 hover:text-red-700 transition duration-150 ease-in-out "
+                  to="/sign-up"
+                >
+                  Register
+                </Link>
+              </p>
+              <p className="text-md mt-3 sm:mt-0">
+                <Link
+                  className="text-blue-500 hover:text-blue-700 transition duration-150 ease-in-out"
+                  to={"/forgot-password"}
+                >
+                  Forgot Password
+                </Link>
+              </p>
+            </div>
+            <button
+              className="w-full py-3 my-6 bg-blue-600 text-xl text-white font-semibold uppercase rounded-lg hover:bg-blue-700 transition duration-200 ease-in-out hover:shadow-lg  active:bg-blue-900"
+              type="submit"
+            >
+              sign in
+            </button>
+            <div className="flex items-center   before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
+              <p className="text-center font-semibold mx-4">OR</p>
+            </div>
+            <SignInWithGoogle />
           </form>
-          <div className="md:flex  md:items-center md:justify-between md:mt-3 mt-3">
-            <p className="text-md">
-              <span className="text-gray-500 ">
-                Don&apos;t have an account?
-              </span>
-              <Link
-                className="text-red-500 ml-2 hover:text-red-700 transition duration-150 ease-in-out "
-                to="/sign-up"
-              >
-                Register
-              </Link>
-            </p>
-            <p className="text-md mt-3 sm:mt-0">
-              <Link
-                className="text-blue-500 hover:text-blue-700 transition duration-150 ease-in-out"
-                to={"/forgot-password"}
-              >
-                Forgot Password
-              </Link>
-            </p>
-          </div>
-          <button
-            className="w-full py-3 my-6 bg-blue-600 text-xl text-white font-semibold uppercase rounded-lg hover:bg-blue-700 transition duration-200 ease-in-out hover:shadow-lg  active:bg-blue-900"
-            type="submit"
-          >
-            sign in
-          </button>
-          <div className="flex items-center   before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
-            <p className="text-center font-semibold mx-4">OR</p>
-          </div>
-          <SignInWithGoogle />
         </div>
       </div>
     </section>
